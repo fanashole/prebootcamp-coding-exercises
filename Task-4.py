299901a0f695001e1dc952935f7dceeaa7b0c5ad
Fana Shole <fanashole@gmail.com>
2020-11-13 13:16:26 +0200

    added Task 4

---
+++ Task-4.py
@@ -0,0 +1,20 @@
+#Task four
+def numbers(x, y):
+    if x == 3 or y == 3:  #check if either number is 3
+        return True
+    else:
+        return False
+
+#Printing the function call out
+print(numbers(3, 40))
+print(numbers(9, 3))
+print(numbers(2, 1))
+print(numbers(5, 8))
+
+'''
+Output:
+True
+True
+False
+False
+'''
