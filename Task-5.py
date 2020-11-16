9ddabd101495b570ca7db97f432bdde4a8da4795
Fana Shole <fanashole@gmail.com>
2020-11-16 16:05:09 +0200

    added Task 5

---
+++ Task-5.py
@@ -0,0 +1,15 @@
+#Task 5
+import math
+
+def area_of_triangle(x, y, z):
+    s = int(x + y + z)/2  #calculating the semi-perimetre
+    area = math.sqrt(s*(s - x) * (s - y) * (s - z))  #calculating the area
+    return(area)
+
+#function call out
+print(area_of_triangle(20, 10, 20))
+
+'''
+Output:
+96.82458365518542
+'''
