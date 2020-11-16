bc364e8f46c70f502b5b03c699540b8a79d040c0
Fana Shole <fanashole@gmail.com>
2020-11-16 16:05:31 +0200

    added Task 6

---
+++ Task-6.py
@@ -0,0 +1,19 @@
+#Task 6
+def max_number(x, y, z):
+    if x > y and x > z:  #check if x has the largest number, if yes then prints x
+        return(x)
+    elif y > x and y > z:  #check if y has the largest number, if yes then prints y
+        return(y)
+    elif z > x and z > y:  #check if z has the largest number, if yes then prints z
+        return(z)
+
+#function call
+print(max_number(10, 15, 20))
+print(max_number(20, 15, 10))
+print(max_number(15, 20, 10))
+'''
+Output:
+20
+20
+20
+'''
